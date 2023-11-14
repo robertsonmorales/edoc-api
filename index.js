09e5d97ee6d858d57e1d0f142ff18dfbd19cba5b
Robertson Morales <moralesso152842@gmail.com>
2023-11-15 01:15:38 +0800

    added origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const paymentController = require('./controllers/paymentController');
 const emailController = require('./controllers/emailController');
 
 const corsOptions = {
-    origin: 'http://localhost:8080',
+    origin: ['http://localhost:8080', 'http://edoc.infinityfreeapp.com'],
     methods: ['GET', 'POST']
 };
 
